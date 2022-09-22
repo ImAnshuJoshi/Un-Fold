e9876d38f6477606ab61b43513b6ae0d0ede8c33
ImAnshuJoshi <anshu.aj.joshi@gmail.com>
2022-09-23 00:12:40 +0530

    asbkas

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,24 +1,27 @@
-const sequelize=require('./index');
-const { DataTypes} = require('sequelize');
+const sequelize = require("./index");
+const { DataTypes } = require("sequelize");
 
-module.exports= ()=>{
-    const Comment=sequelize.define('comments',{
-        id:{
-            type:DataTypes.UUID,
-            defaultValue:DataTypes.UUIDV4,
-            primaryKey:true,
-        },
-        content:{
-            type:DataTypes.STRING,
-        },
-        },
+module.exports = () => {
+  const Comment = sequelize.define(
+    "comments",
     {
-        freezeTableName: true,
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      content: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      freezeTableName: true,
     }
-    );
-    Comment.sync({ force: false })
-    .then(() => {
-        console.log('yes re-sync done!')
-    })
-    return Comment;
-}
+  );
+  Comment.sync({ force: false }).then(() => {
+    console.log("yes re-sync done!");
+  });
+  return Comment;
+};
+
+// Language: javascript
