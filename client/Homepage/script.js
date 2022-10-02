ee923de9131dfbee7aa3558d8ad2b8e1e5ae67b4
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-10-03 02:37:06 +0530

    homepage updated

--- client/Homepage/script.js
+++ client/Homepage/script.js
@@ -1,14 +1,18 @@
 function leftScroll() {
-    const left = document.querySelector(".scroll-images");
-    left.scrollBy(-200, 0);
+  const left = document.querySelector(".scroll-images");
+  left.scrollBy(-200, 0);
 }
 function rightScroll() {
-    const right = document.querySelector(".scroll-images");
-    right.scrollBy(200, 0);
+  const right = document.querySelector(".scroll-images");
+  right.scrollBy(200, 0);
 }
 
-
-const blogz = (img,title,content,user)=>`<a href='../User/index.html' style="text-decoration:none;"><div class="blog-details">
+const blogz = (
+  img,
+  title,
+  content,
+  user
+) => `<a href='index.html' style="text-decoration:none;"><div class="blog-details">
             <div class="child">
             <img src = ${img} alt="" />
             </div>
@@ -19,26 +23,53 @@ const blogz = (img,title,content,user)=>`<a href='../User/index.html' style="tex
                 <li>Fun</li>
             </ul>
             </div>
+            <a href="../User/index.html?id=${user.id}" style="height: 0;">
+                <img class="author" src=${user.imageurl} alt="author-image">
+            </a>
             <div class="desc">${title}</div>
             <div class="desc2">
             ${content}
             </div>
-            <a>${user.fname} ${user.lname}</a>
-            </div></a>`
-let blogsj;            
-window.onload=
-    async() => {
-    const blogs=await fetch("http://192.168.2.100:3000/api/blog/getAllBlogs", {
-        method: "GET",
-        headers: {
+            </div></a>`;
+let blogsj;
+const finduser = async (id) => {
+  const user = await fetch(
+    "http://192.168.137.103:3000/api/user/getuserinfo?" +
+      new URLSearchParams({ id: id }),
+    {
+      method: "GET",
+      headers: {
         "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        mode: "cors",
-        credentials: "same-origin",
-    })
-        blogsj=await blogs.json();
-        blogsj.map((b)=>{
-            document.getElementById('i1').insertAdjacentHTML('afterbegin',blogz(b.imageurl,b.title,b.content));
-        })
-    };
+      },
+      mode: "cors",
+      credentials: "same-origin",
+    }
+  );
+  const userj = await user.json();
+  return userj.user;
+};
+window.onload = async () => {
+  const blogs = await fetch(
+    "http://192.168.137.103:3000/api/blog/getAllBlogs",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      mode: "cors",
+      credentials: "same-origin",
+    }
+  );
+  blogsj = await blogs.json();
+  blogsj.map(async (b) => {
+    const user = await finduser(b.userId);
+    document
+      .getElementById("i1")
+      .insertAdjacentHTML(
+        "afterbegin",
+        blogz(b.imageurl, b.title, b.content, user)
+      );
+  });
+};
--- client/User/script.js
+++ client/User/script.js
@@ -14,3 +14,90 @@
             document.getElementById('i1').insertAdjacentHTML('afterbegin',blogz(b.imageurl,b.title,b.content));
         })
     }; */
+    let user;
+const userprofile =(u) => 
+                `<div class="profile-user-img">
+                <img src=${u.imageurl} alt="" />
+                </div>
+                <div class="profile-user-name">
+                <h1>${u.firstName} ${u.lastName}</h1>
+                </div>
+                <div class="user-followers">
+                  <div class="followers stats">
+                    <div class="count">150 </div><div>Followers</div>
+               </div>
+                  <div class="followings stats">
+                    <div class="count">100 </div><div>Following</div>
+                  </div>
+                  <div class="blogs stats">
+                    <div class="count">100 </div><div>Blogs</div>
+                  </div>
+                </div>`;
+                const blogz = (
+                    img,
+                    title,
+                    content
+                  ) => `<a href='../User/index.html' style="text-decoration:none;"><div class="follow-cards column">
+                  <div class="blog-details">
+                              <div class="child">
+                              <img src = ${img} alt="" />
+                              </div>
+                              <div class="tag-wrap">
+                              <ul class="tags">
+                                  <li>Design</li>
+                                  <li>Coding</li>
+                                  <li>Fun</li>
+                              </ul>
+                              </div>
+                              <a href="../User/index.html?id=${user.id}" style="height: 0;">
+                                  <img class="author" src=${user.imageurl} alt="author-image">
+                              </a>
+                              <div class="desc">${title}</div>
+                              <div class="desc2">
+                              ${content}
+                              </div>
+                              </div></div></a>`;
+                  let blogsj;
+window.onload = async () => {
+  const queryParamsString = window.location.search?.substring(1);
+  const id = queryParamsString?.substring(3);
+  const userj = await fetch(
+    "http://192.168.137.103:3000/api/user/getuserinfo?" +
+      new URLSearchParams({ id: id }),
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      mode: "cors",
+      credentials: "same-origin",
+    }
+  );
+  const blogs = await fetch(
+    "http://192.168.137.103:3000/api/blog/getAllBlogs",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      mode: "cors",
+      credentials: "same-origin",
+    }
+  );
+  blogsj = await blogs.json();
+  user = await userj.json();
+  user=user.user;
+  blogsj.map(async (b) => {
+    document
+      .getElementsByClassName('latest-cards row')[0]
+      .insertAdjacentHTML(
+        "afterbegin",
+        blogz(b.imageurl, b.title, b.content)
+      );
+  });
+  document.getElementsByClassName('profile-user-wrap')[0].insertAdjacentHTML("afterbegin",userprofile(user)); 
+  document.getElementsByClassName('user-desc')[0].insertAdjacentHTML("afterbegin",user.about); 
+ 
+};
