8bea101564859f0254f4e4f296852e7f31cb6c34
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-10-07 01:56:12 +0530

    hamburger doing

--- client/Homepage/script.js
+++ client/Homepage/script.js
@@ -17,17 +17,6 @@ function closeMenu() {
 }
 
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
 const blogz = (
   img,
   title,
