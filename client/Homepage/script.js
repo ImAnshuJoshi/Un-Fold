e4f265fc32977240ecbf4bbe6ad7b3c15c2f11a8
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-10-07 01:34:49 +0530

    hamburger doing

--- client/Homepage/script.js
+++ client/Homepage/script.js
@@ -1,10 +1,31 @@
-function leftScroll() {
-  const left = document.querySelector(".scroll-images");
-  left.scrollBy(-200, 0);
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
-function rightScroll() {
-  const right = document.querySelector(".scroll-images");
-  right.scrollBy(200, 0);
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
 
 const blogz = (
