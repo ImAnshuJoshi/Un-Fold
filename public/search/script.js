2be1f02b3ef7ec1fd875b7efb86d3526e568fb25
Aaroh Srivastava <aarohsjcian1@gmail.com>
2022-10-23 00:09:19 +0530

    changes made

--- public/search/script.js
+++ public/search/script.js
@@ -143,7 +143,8 @@ const blogCard = (
   blog,
   user,
   tags,K
-) => `<a href='../Blog-opening/blog-opening.html?id=${blog.id}' style="text-decoration:none;"><div class="blog-details">
+) => `<a href='../Blog-opening/blog-opening.html?id=${blog.id}' style="text-decoration:none;">
+          <div class="blog-details">
             <div class="img-container">
             <img src = ${blog.imageurl} alt="" />
             </div>
@@ -190,7 +191,7 @@ let word = "";
 const searchtheword = () => {
   const searchword = document.querySelector(".search-input").value;
   document.getElementById("search-span").innerHTML =
-    "Results for " + searchword;
+    "Results for " + "'" + searchword + "'";
   console.log(searchword);
   word = searchword;
   searchBlogs();
