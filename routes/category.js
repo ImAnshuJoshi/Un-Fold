cb888073f309a88411c2c5960f159ce5d435fda6
Aaroh Srivastava <aarohsjcian1@gmail.com>
2022-10-25 09:44:38 +0530

    minor fix

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,7 @@ const express = require('express')
 const upload = require('../Config/multerConfig')
 const router = express.Router()
 const catcontrollers = require('../Controllers/category')
+const auth = require('../Middlewares/tokenauth');
 
 router.get('/getallcategories', catcontrollers.getallcategories)
 
